f63ac6af0032465cd037bfd3ff99ddb5fbae14f8
deepak <depmeena5@gmail.com>
2023-03-30 15:34:21 +0530

    Learning JPA and mySQL

---
+++ 03-hibernate-jpa/02-DAO-impl/src/main/java/com/deepak/springDemo/SpringDemoApplication.java
@@ -0,0 +1,58 @@
+package com.deepak.springDemo;
+
+import com.deepak.springDemo.dao.StudentDAO;
+import com.deepak.springDemo.dao.StudentDAOImpl;
+import com.deepak.springDemo.entity.Student;
+import jakarta.persistence.TypedQuery;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
+		return runner -> {
+			createUser(studentDAO);
+			getUser(studentDAO, 8);
+			printQuery(studentDAO, "From Student");
+		};
+	}
+
+	public void printQuery(StudentDAO studentDAO,String query){
+		List<Student> students = studentDAO.queryResults(query);
+		for (Student student : students) {
+			System.out.println(student);
+		}
+
+	}
+	public void getUser(StudentDAO studentDAO, Integer id){
+		Student student = studentDAO.findByID(id);
+		if (student == null){
+			System.out.println("User Not Found");
+			return;
+		}
+		System.out.println(student);
+	}
+
+	public void createUser(StudentDAO studentDAO){
+		//creating a new user
+		System.out.println("Creating a new user");
+		Student student = new Student("paul", "Joe", "paul@gmail.com");
+
+		//save the student object
+		System.out.println("Saving the student object");
+		studentDAO.save(student);
+
+		//printing the student id
+		System.out.println("Saved student, Generated Id is: " + student.getId());
+	}
+}
